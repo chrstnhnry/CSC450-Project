73fb89ce36c9d208796fbdd4e5a8463ade8d802f
chrstnhnry <chrstnhnry@gmail.com>
2022-02-11 16:51:24 -0600

    made functions

--- project.py
+++ project.py
@@ -4,12 +4,20 @@ import csv
 excel_file = sys.argv[1]
 
 matrix = []
+node_names = []
 
-with open(excel_file, "r") as this_csv_file:
-    csv_reader = csv.reader(this_csv_file)
+with open(excel_file, "r") as csv_file:
+    csv_reader = csv.reader(csv_file)
     next(csv_reader, 1)
     for line in csv_reader:
         
         matrix.append(line[1:])
-        
-print(matrix)
+        node_names.append(line[0])
+
+node = input("Please, provide the source node: ")
+
+def main():
+    
+#cost between two nodes that are given
+def cost(n1, n2):
+    
