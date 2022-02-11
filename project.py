3f8f88b1cf9fdcb3ace41143664d9e7e5e3fc328
chrstnhnry <chrstnhnry@gmail.com>
2022-02-11 16:07:07 -0600

    read csv file
    ...

--- project.py
+++ project.py
@@ -3,7 +3,13 @@ import csv
 
 excel_file = sys.argv[1]
 
+matrix = []
+
 with open(excel_file, "r") as this_csv_file:
-    this_csv_reader = csv.reader(this_csv_file, delimiter=",")
-    header = next(this_csv_reader)
-    print(header)
+    csv_reader = csv.reader(this_csv_file)
+    next(csv_reader, 1)
+    for line in csv_reader:
+        
+        matrix.append(line[1:])
+        
+print(matrix)
