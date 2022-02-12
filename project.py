66773967f14dbd8829e8ca7c9a1773e3f14ace3b
chrstnhnry <chrstnhnry@gmail.com>
2022-02-12 09:53:07 -0600

    Added two more lists and a function

--- project.py
+++ project.py
@@ -14,10 +14,36 @@ with open(excel_file, "r") as csv_file:
         matrix.append(line[1:])
         node_names.append(line[0])
 
-node = input("Please, provide the source node: ")
-
 def main():
+
+    shortest_distance = [9999, 9999, 9999, 9999, 9999, 9999]
+
+    node = input("Please, provide the source node: ")
+
+    previous_index = [node, node, node, node, node, node]
+
+    print("Shortest path tree for node " + node + ": ")
+
+    #the index of row of node
+    row = node_names.index(node)
+
+    for i in range(len(node_names)):
+        shortest_distance[i] = cost(shortest_distance[i], int(matrix[row][i]))
+
+    print(sorted(shortest_distance)[1])
     
+    print(shortest_distance)
+    print(previous_index)
+        
+
+    
+
 #cost between two nodes that are given
 def cost(n1, n2):
+    if n1 > n2:
+        return n2
+    else:
+        return n1
+
+main()
     
