e9f9fbefccef205f92937b711dba25a2e882b084
chrstnhnry <chrstnhnry@gmail.com>
2022-02-12 11:01:41 -0600

    added some more stuff

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ excel_file = sys.argv[1]
 
 matrix = []
 node_names = []
-
+unvisited = []
 with open(excel_file, "r") as csv_file:
     csv_reader = csv.reader(csv_file)
     next(csv_reader, 1)
@@ -13,6 +13,7 @@ with open(excel_file, "r") as csv_file:
         
         matrix.append(line[1:])
         node_names.append(line[0])
+        unvisited.append(line[0])
 
 def main():
 
@@ -22,28 +23,36 @@ def main():
 
     previous_index = [node, node, node, node, node, node]
 
+    visited = []
+
     print("Shortest path tree for node " + node + ": ")
 
     #the index of row of node
     row = node_names.index(node)
-
+    print(row)
+    visited.append(node_names[row])
+    del unvisited[row]
     for i in range(len(node_names)):
-        shortest_distance[i] = cost(shortest_distance[i], int(matrix[row][i]))
-
-    print(sorted(shortest_distance)[1])
-    
-    print(shortest_distance)
-    print(previous_index)
+        shortest_distance[i] = int(matrix[row][i])
         
 
-    
+    row = shortest_distance.index(sorted(shortest_distance)[1])
+    print(row)
+    visited.append(node_names[row])
+    del unvisited[row-1]
+    for i in range(len(node_names)):
+        
+        cost = sum(int(matrix[row][i]), shortest_distance[row])
+                   
+        if (shortest_distance[i] > cost):
+            shortest_distance[i] = cost
 
-#cost between two nodes that are given
-def cost(n1, n2):
-    if n1 > n2:
-        return n2
-    else:
-        return n1
+    print(visited)
+    print(unvisited)
+    print(node_names)
+            
+def sum(n1, n2):
+    return n1 + n2
 
 main()
     
