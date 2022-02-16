ac70d42fa5a9549d2d44f19d8bed71dfdaefa6f1
chrstnhnry <chrstnhnry@gmail.com>
2022-02-16 17:04:02 -0600

    updates

--- project.py
+++ project.py
@@ -2,13 +2,13 @@ import sys
 import csv
 
 excel_file = sys.argv[1]
-
+row_cost = []
+total_cost_list = []
 matrix = []
 node_names = []
 unvisited = []
 visited = []
-total_cost_list = []
-previous_index = ['', '', '', '', '', '']
+previous_index = []
 shortest_distance = [9999, 9999, 9999, 9999, 9999, 9999]
 
 with open(excel_file, "r") as csv_file:
@@ -19,6 +19,64 @@ with open(excel_file, "r") as csv_file:
         matrix.append(line[1:])
         node_names.append(line[0])
         unvisited.append(line[0])
+
+def main2():
+
+    current_vertex = node_names.index(node)
+    previous_vertex = current_vertex
+
+    #row
+    for i in range(len(node_names)):
+        
+        visited.append(current_vertex)
+        unvisited.remove(node_names[current_vertex])
+        
+        #column
+        for j in range(len(node_names)):
+            
+            if int(matrix[current_vertex][j] < 9999:
+                total_cost = totalCost(current_vertex, previous_vertex, j, visited)
+                   
+            else:
+                total_cost = 9999
+                
+            row_cost.append(total_cost)
+            
+            if total_cost < shortest_distance[j]:
+                shortest_distance[i] = total_cost
+        
+        previous_vertex = current_vertex
+                   
+        #set current vertex
+        while True:
+            
+            if visited.count(row_cost.index(min(a))) > 0:
+                current_vertex = row_cost.index(min(a))
+                break
+        
+            else:
+                row_cost.index(min(a)) = 9999
+
+        #clear row total cost
+        row_cost.clear()
+        
+
+def totalCost(current_vertex, previous_vertex, j, visited):
+    if len(visited) == 1:
+        return int(matrix[current_vertex][j])
+
+    else:
+        return int(matrix[][]) + totalCost(curent_vertex, previous_vertex, j, )
+
+
+
+
+
+
+
+
+
+
 
 def main():
 
@@ -40,6 +98,8 @@ def main():
     previous_vertex = current_vertex
     current_vertex = shortest_distance.index(sorted(total_cost_list)[1])
 
+    print(shortest_distance)
+
     for i in range(len(node_names)):
 
         total_cost = int(matrix[current_vertex][i]) + int(matrix[previous_vertex][current_vertex])
@@ -49,6 +109,8 @@ def main():
     previous_vertex = current_vertex
     current_vertex = 1
 
+    print(shortest_distance)
+
     for i in range(len(node_names)):
 
         total_cost = int(matrix[current_vertex][i]) + int(matrix[previous_vertex][current_vertex]) + int(matrix[0][previous_vertex])
@@ -57,6 +119,8 @@ def main():
 
     previous_vertex = current_vertex
     current_vertex = 4
+
+    print(shortest_distance)
 
     for i in range(len(node_names)):
 
